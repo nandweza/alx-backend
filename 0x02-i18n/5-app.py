ad4309f87970e841137fa46e044d3eebc5cd7c47
nandweza <allannandweza@gmail.com>
2022-07-19 16:25:02 +0300

    fixed code

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
-"""Mock logging in"""
-
-from typing import Union
+""" Module for trying out Babel i18n """
+from flask_babel import Babel, _
 from flask import Flask, render_template, request, g
-from flask_babel import Babel
+from typing import Union
 
 app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
 
 
 class Config(object):
-    """Babel configuration"""
+    """ Configuration Class for Babel """
+
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
-
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -28,35 +27,41 @@ users = {
 
 
 def get_user() -> Union[dict, None]:
-    """Get user from session as per variable"""
+    """ Returns a user dictionary or
+    None if the ID cannot be found or
+    if login_as was not passed.
+    """
+
     try:
-        login_as = request.args.get('login_as', None)
+        login_as = request.args.get("login_as")
         user = users[int(login_as)]
     except Exception:
         user = None
 
+    return user
+
 
 @app.before_request
 def before_request():
-    """OPerations b4 requests"""
+    """ Operations that happen before any request """
     user = get_user()
     g.user = user
 
 
-@app.route("/", methods=['GET'], strict_slashes=False)
-def index() -> str:
-    """render template for Babel usage"""
-    return render_template('5-index.html')
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world() -> str:
+    """Renders a Basic Template for Babel Implementation"""
+    return render_template("5-index.html")
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """determine the best match with supported lang."""
-    locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
+    """Select a language translation to use for that request"""
+    locale = request.args.get("locale")
+    if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
