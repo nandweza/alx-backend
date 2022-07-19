7cf6494f62e83c8ee019e75b923796368f736512
nandweza <allannandweza@gmail.com>
2022-07-19 16:26:35 +0300

    updates

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
-""" Module for trying out Babel i18n """
-from flask_babel import Babel, _
-from flask import Flask, render_template, request, g
+"""Mock logging in"""
+
 from typing import Union
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
 
 app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
 
 
 class Config(object):
-    """ Configuration Class for Babel """
-
+    """Babel configuration"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
+
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -27,41 +28,35 @@ users = {
 
 
 def get_user() -> Union[dict, None]:
-    """ Returns a user dictionary or
-    None if the ID cannot be found or
-    if login_as was not passed.
-    """
-
+    """Get user from session as per variable"""
     try:
-        login_as = request.args.get("login_as")
+        login_as = request.args.get('login_as', None)
         user = users[int(login_as)]
     except Exception:
         user = None
 
-    return user
-
 
 @app.before_request
 def before_request():
-    """ Operations that happen before any request """
+    """OPerations b4 requests"""
     user = get_user()
     g.user = user
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def hello_world() -> str:
-    """Renders a Basic Template for Babel Implementation"""
-    return render_template("5-index.html")
+@app.route("/", methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """render template for Babel usage"""
+    return render_template('5-index.html')
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """Select a language translation to use for that request"""
-    locale = request.args.get("locale")
-    if locale and locale in app.config['LANGUAGES']:
+    """determine the best match with supported lang."""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
